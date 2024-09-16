bfcd85aafef323b01e44f99051056a6e08e12d05
juliacuri <163016057+juliacuri@users.noreply.github.com>
2024-09-16 12:37:23 -0300

    ...dia.16

--- perguntas.js
+++ perguntas.js
@@ -12,8 +12,8 @@ criaCartao(
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Qual a cor do céu?',
+    'o céu é azul'
 )
 
 criaCartao(
